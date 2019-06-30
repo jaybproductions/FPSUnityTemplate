a4fc0669fc7dd9ae0c562ebace03bf71e9cec61e
jaybproductions <jaybgameproductions@gmail.com>
2019-06-30 11:08:19 -0400

    Initial check-in of module FPS Project

---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverUI : MonoBehaviour
+{
+
+
+    public Canvas gameoverUI;
+    private GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        gameoverUI.enabled = false;
+        player = GameObject.FindWithTag("Player");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if(PlayerStats.health <= 0)
+        {
+
+
+            GameOver();
+
+
+        }
+        
+    }
+
+
+    void GameOver()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        gameoverUI.enabled = true;
+
+
+        GameObject.FindWithTag("OverlayUI").SetActive(false);
+        GameObject.FindWithTag("Player").GetComponent<PlayerMotor>().enabled = false;
+        GameObject.FindWithTag("WeaponSlot").SetActive(false);
+
+
+    }
+
+    public void Retry()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+       
+
+
+    }
+
+    public void MainMenu ()
+    {
+
+        SceneManager.LoadScene(0);
+
+
+    }
+
+
+    public void Quit()
+    {
+
+        Application.Quit();
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerStats : MonoBehaviour
+{
+    public Slider healthBar;
+    public Text moneyText;
+
+
+    public static int money;
+    public static int health;
+    public int startHealth = 100;
+    public int startMoney = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = startHealth;
+        money = startMoney;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moneyText != null)
+        
+        {
+            moneyText.text = "$" + money;
+        }
+
+        if (healthBar != null)
+        {
+            healthBar.value = health;
+        }
+
+
+
+
+
+    }
+
+
+
+}
